bee509fe4f3229eb6e1683f875e8f14eb4f7fa89
Zero Kim <103815045+Winighter@users.noreply.github.com>
2025-07-22 16:03:39 +0900

    Delete config directory

--- config/enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class LockState(Enum):
-    READY_TO_OPEN = 0        # 시스템이 새로운 포지션을 열 준비가 된 상태
-    AWAITING_SELL_SIGNAL = 1      # 다음 신호를 기다리는 상태
-    IGNORE_NEXT_SIGNAL = -1  # 다음 신호를 무시해야 하는 상태 (예: 수익 실현 후)
