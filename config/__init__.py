af15913e52023a322d822ce165d163b695b24bb6
Zero Kim <103815045+Winighter@users.noreply.github.com>
2025-08-19 10:35:03 +0900

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+# Discord Message
+from .msg import Message
+
+# Indicators
+from .indicators import Indicators
+
+# Strategies
+from .strategies import Strategies
---
+++ read.md
@@ -0,0 +1,5 @@
+# Ubuntu
+
+python3 main.py binance_son.key
+python3 main.py binance_dad.key
+python3 main.py binance_mom.key
