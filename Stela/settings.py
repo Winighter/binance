be71b2756b605811f8e62a176f51967e1308ca5a
Zero Kim <103815045+Winighter@users.noreply.github.com>
2026-01-16 15:46:07 +0900

    Add files via upload

---
+++ Stela/settings.py
@@ -0,0 +1,15 @@
+from src.shared.enums import KlineInterval
+
+
+FUTURES_TRADING_ASSET = 'USDT'
+SYMBOL = "XRPUSDT" # "ETHUSDT"
+LEVERAGE = 10
+
+# KLINE_INTERVAL
+KLINE_INTERVAL = KlineInterval.MINUTE_15
+
+
+### ENABLES ###
+ENABLE_ORDER = True # 실거래 주문 실행 여부
+ENABLE_SIMULATION = False # 백테스팅 시뮬레이션 실행 여부 (주의: ENABLE_ORDER가 True일 땐 False여야 함)
+ENABLE_DISCORD_ALERTS = True  # 디스코드 알림 활성화 여부
